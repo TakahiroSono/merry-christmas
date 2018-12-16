3b33cd6fb15054bed1ef054be97c593f21635095
TakahiroSono <taka.sono889@gmail.com>
2018-12-16 23:07:33 +0900

    add component presents

---
+++ src/app/presents/presents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-presents',
+  templateUrl: './presents.component.html',
+  styleUrls: ['./presents.component.scss']
+})
+export class PresentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
